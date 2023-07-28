7c2a6cf111b0a0153367cb4621819f0a5bd53416
Aryan <aryan1bhokare@gmail.com>
2023-07-28 08:39:43 +0530

    a few changes

--- pages/index.js
+++ pages/index.js
@@ -80,7 +80,7 @@ export default function Home() {
             <div className='grid grid-cols-1 md:grid-cols-2 p-2 py-3 gap-8'>
               <div>
                 <h1 className=' text-lg font-bold dark:text-white'>Get to know me!</h1>
-                <h2 className=' dark:text-white'>I'm a Frontend Web Developer building the Front-end of Websites and Web Applications that leads to the success of the overall product. Check out some of my work in the Projects section.
+                <h2 className=' dark:text-white'>I'm a Full Stack Web Developer building the Functionalities of Websites and Web Applications that leads to the success of the overall product. Check out some of my work in the Projects section.
 
                 I also like sharing content related to the stuff that I have learned over the years in Web Development so it can help other people of the Dev Community. Feel free to Connect or Follow me on my Linkedin where I post useful content related to Web Development and Programming
 
